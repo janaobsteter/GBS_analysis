146e9f71fdc86e3dc57e38796646559fed5bdbc4
Črt Zega <crt.zega@gmail.com>
2025-09-02 18:27:03 +0200

    Add script to create directory structure

---
+++ stacks_pipeline/folders.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p \
+    alignments \
+    fastq \
+    fastq_trimmed \
+    gstacks \
+    logs \
+    metadata \
+    populations \
+    samples \
+    samples_merged \
+    tags 
